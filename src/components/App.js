197edd868f16fa31f780f83265da595a8e68ac5d
[Prerit Sahai] <[preritsahai13@gmail.com]>
2025-08-14 00:47:25 +0530

    Neww Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const App = () => {
+function App() {
   return (
     <div id="main">
       <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     </div>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; // Make sure this matches the filename exactly
 
 ReactDOM.render(<App />, document.getElementById('root'));
