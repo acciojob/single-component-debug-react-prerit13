3a71d2febc1eccbae562f89723efcb1df8821357
[Prerit Sahai] <[preritsahai13@gmail.com]>
2025-08-14 00:33:55 +0530

    New Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,11 @@
-<p></p>
+import React from 'react';
+
+const App = () => {
+  return (
+    <div id="main">
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  );
+};
+
+export default App;
