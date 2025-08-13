5eac01141c0e71293afd1b1c5ceaad2ef00ac08d
[Prerit Sahai] <[preritsahai13@gmail.com]>
2025-08-14 01:31:57 +0530

    Ayush

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // ✅ Must exactly match the file name
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
